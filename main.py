9cdb9a2ec6c6f6b93a8c20b2271745b62b8db5ec
ArdenchikPlus <raskovalov132@gmail.com>
2026-06-09 12:25:52 +0300

    last version

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import random
 import string
 all_chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + "!@#$%"
-print(all_chars)
+password = ""
+enter = int(input("Enter the number of characters in the password (number):"))
+for i in range(enter):
+    random_char = random.choice(all_chars)
+    password = password + random_char
+print("Your unique password:" + password)
